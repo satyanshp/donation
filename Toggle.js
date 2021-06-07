a948e9926489a73c9bf672d0cd4aa2893afe9126
satyanshp <74150346+satyanshp@users.noreply.github.com>
2021-06-07 18:50:50 +0530

    Add files via upload

---
+++ Toggle.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName("toggle")[0];
+const navLinks = document.getElementsByClassName("nav-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
